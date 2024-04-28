9fbdd8beea0f02ed8d548bf2a9a1f1545eabc153
Matheus A. Oliveira <matheus.adeoliveira11@gmail.com>
2024-04-28 02:42:14 -0300

    feat: add course students and function to add them

--- src/domain/entities/course.ts
+++ src/domain/entities/course.ts
@@ -1,4 +1,6 @@
+import { CannotCreateError } from "src/errors/cannot-create";
 import { Entity } from "./base-entity";
+import { Student } from "./student";
 import { Teacher } from "./teacher";
 
 export interface CourseProps {
@@ -6,14 +8,29 @@ export interface CourseProps {
   name: string;
   description: string;
   banner: string; // TODO: implement blob
+  students?: Array<Student>;
 }
 
 export class Course extends Entity<CourseProps> {
   constructor(props: CourseProps) {
-    super(props);
+    super({
+      ...props,
+      students: props.students || [],
+    });
   }
 
   edit(data: Partial<Omit<CourseProps, "teacher">>) {
     this.props = { ...this.props, ...data };
+  }
+
+  addStudent(student: Student): void {
+    const existentStudentIdx = this.props.students.findIndex(
+      (item) => item.props.username === student.props.username,
+    );
+
+    if (existentStudentIdx > -1)
+      throw new CannotCreateError("Student already exists");
+
+    this.props.students.push(student);
   }
 }
