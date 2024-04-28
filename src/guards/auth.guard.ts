73ace362ff2e5ddd70605e5fad85181c2dce0642
Matheus A. Oliveira <matheus.adeoliveira11@gmail.com>
2024-04-28 18:25:47 -0300

    feat: add auth guard

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,65 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  UnauthorizedException,
+  UseGuards,
+} from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+import { JwtService } from "@nestjs/jwt";
+import { UserRole } from "src/graphql/inputs/auth/register.input";
+import { AppContext, ContextUser } from "src/types/app-context";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  #jwtService = new JwtService();
+
+  constructor(
+    private allowedRoles?: UserRole[],
+    private errorMessage?: string,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const ctx = GqlExecutionContext.create(context).getContext<AppContext>();
+    const authorizationHeader = ctx.request.headers.authorization;
+
+    if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+      return false;
+    }
+
+    const token = authorizationHeader.slice(7);
+    try {
+      const decoded = this.#jwtService.verify<ContextUser>(token);
+
+      if (this.allowedRoles && !this.allowedRoles.includes(decoded.role)) {
+        throw new ForbiddenException(this.errorMessage);
+      }
+
+      ctx.user = decoded;
+      return true;
+    } catch (error) {
+      if (error instanceof ForbiddenException) throw error;
+    }
+
+    throw new UnauthorizedException();
+  }
+}
+
+export function UseAuthGuard(
+  errorMessage?: string,
+): ReturnType<typeof UseGuards>;
+export function UseAuthGuard(
+  allowedRoles?: UserRole[],
+  errorMessage?: string,
+): ReturnType<typeof UseGuards>;
+export function UseAuthGuard(
+  allowedRolesOrMessage?: UserRole[] | string,
+  errorMessage?: string,
+): ReturnType<typeof UseGuards> {
+  if (typeof allowedRolesOrMessage === "string") {
+    return UseGuards(new AuthGuard(undefined, allowedRolesOrMessage));
+  }
+
+  return UseGuards(new AuthGuard(allowedRolesOrMessage, errorMessage));
+}
