0dce32098807770bba6fb2b57019155116e58966
Matheus A. Oliveira <matheus.adeoliveira11@gmail.com>
2024-04-28 18:40:36 -0300

    feat: add jwt variables

---
+++ src/env.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  JWT_ACCESS_TOKEN_SECRET: z.string(),
+  JWT_ACCESS_TOKEN_EXPIRES_IN: z.string().default("30m"),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  throw new Error("Invalid environment variables");
+}
+
+export const env = parsedEnv.data;
