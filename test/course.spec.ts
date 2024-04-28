fd3c84b7f1216a967dfaf7a146cfe2f8955eccf6
Matheus A. Oliveira <matheus.adeoliveira11@gmail.com>
2024-04-28 03:14:25 -0300

    test: add tests related to the lesson methods of the course entity

--- test/course.spec.ts
+++ test/course.spec.ts
@@ -1,14 +1,17 @@
 import { afterEach } from "node:test";
 import { Course } from "src/domain/entities/course";
+import { Lesson } from "src/domain/entities/lesson";
 import { Student } from "src/domain/entities/student";
 import { Teacher } from "src/domain/entities/teacher";
 import { CourseRepository } from "src/repositories/course.repository";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 
 describe("Course", () => {
   let repository = new CourseRepository();
 
-  afterEach(() => (repository = new CourseRepository()));
+  beforeEach(() => {
+    repository = new CourseRepository();
+  });
 
   it("should create a course and edit its infos", () => {
     const teacher = new Teacher({
@@ -21,6 +24,7 @@ describe("Course", () => {
       description: "Test course n1",
       banner: "test_banner.jpg",
       teacher,
+      lessons: [new Lesson({ content: "123" })],
     });
 
     expect(course.props.name).toStrictEqual("Course 1");
@@ -44,6 +48,11 @@ describe("Course", () => {
       description: "Test course n1",
       banner: "test_banner.jpg",
       teacher,
+      lessons: [
+        new Lesson({
+          content: "content",
+        }),
+      ],
     });
     const student1 = new Student({
       name: "Jane Doe",
@@ -81,5 +90,51 @@ describe("Course", () => {
 
     expect(repoCourse.props.students.length).toStrictEqual(1);
     expect(repoCourse.props.students[0]).toStrictEqual(student2);
+  });
+
+  it("should add and remove lessons to the course", () => {
+    const lesson1 = new Lesson({
+      content: "content",
+    });
+    const lesson2 = new Lesson({
+      content: "another content",
+    });
+    const teacher = new Teacher({
+      name: "John Doe",
+      password: "123123",
+      username: "johhnydoe.teacher",
+    });
+    const course = new Course({
+      name: "Course 1",
+      description: "Test course n1",
+      banner: "test_banner.jpg",
+      teacher,
+      lessons: [lesson1],
+    });
+
+    repository.store(course);
+
+    let repoCourse = repository.get(course.id);
+
+    expect(repoCourse.id).toStrictEqual(course.id);
+    expect(repoCourse.props.lessons.length).toStrictEqual(1);
+
+    repoCourse.addLesson(lesson2);
+
+    repository.update(repoCourse.id, repoCourse.props);
+
+    repoCourse = repository.get(course.id);
+
+    expect(repoCourse.props.lessons.length).toStrictEqual(2);
+    expect(repoCourse.props.lessons[1].id).toStrictEqual(lesson2.id);
+
+    repoCourse.removeLesson(lesson2.id);
+
+    repository.update(repoCourse.id, repoCourse.props);
+
+    repoCourse = repository.get(course.id);
+
+    expect(repoCourse.props.lessons.length).toStrictEqual(1);
+    expect(repoCourse.props.lessons[0]).toStrictEqual(lesson1);
   });
 });
